c4ffdfa2dc18ca4729dbbda4120f904b6fbda3c1
James Greiner <jamesdgreiner@icloud.com>
2026-03-05 21:38:08 -0800

    Initial Commit

---
+++ PaywallBumper/PaywallBumperApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaywallBumperApp.swift
+//  PaywallBumper
+//
+//  Created by James Greiner on 3/5/26.
+//
+
+import SwiftUI
+
+@main
+struct PaywallBumperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
